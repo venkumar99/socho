fbe11bf4f84559daa4e8384cd05f730a20200f12
Jitendra Sah <jsah9105@gmail.com>
2018-09-20 06:38:03 -0500

    File deleted

--- Trinity/configureStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './redux/reducers/index';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-
-export default function configureStore() {
-    let store = createStore(reducers, applyMiddleware(thunk, logger));
-}
