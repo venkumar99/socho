0594e4fb8e01b76ac00f545243cab867def233a9
Jitendra Sah <jsah9105@gmail.com>
2018-12-03 07:12:18 -0600

    Change for email

--- controller/addUserController.js
+++ controller/addUserController.js
@@ -9,16 +9,13 @@ import User from '../models/user';
 import AccountAccessList from '../models/accountAccessList';
 import AccountList  from '../models/accountList';
 
-//const sgMail = require('@sendgrid/mail');
 
 var jwtOptions ={};
-
-//JWTOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOptions.secretOrKey = CONFIG.jwt_secret_key;
 
 var addUserController = {};
 
-//Add User
+//Send email to user to get account approved
 addUserController.addUser = function(req,res) { 
     var note_dateUTC = moment.utc().format('YYYY-MM-DD HH:mm:ss');
     let userInfo = req.body.userDetail;
@@ -71,11 +68,9 @@ addUserController.addUser = function(req,res) {
 
 //Validate Email
 addUserController.validateEmail = function(req, res) {
-    console.log('email: ', req.params.token)
 
     try {
         const tokenVerify = jwt.verify(req.params.token, CONFIG.jwt_secret_key);
-        console.log('verify email: ', tokenVerify)
         res.sendfile(path.join('./public/emailVerify.html'));
     } catch(e) {
         console.log('error', e)
@@ -85,13 +80,12 @@ addUserController.validateEmail = function(req, res) {
 //Email response
 addUserController.emailResponse = function(req, res) {
 
-    try {
-        const tokenVerify = jwt.verify(req.params.token, CONFIG.jwt_secret_key, function(err, decoded) {
-            console.log('tokenVerify', decoded);
-          });
-        
+    try { 
         if(req.params.response === 'accept') {
-            res.status(200).json('<h3>Thank you for authorization</h3>');
+            const tokenVerified = jwt.verify(req.params.token, CONFIG.jwt_secret_key);
+            if(tokenVerified) {
+                addUserController.updateAccount(tokenVerified, res);
+            }
         } else {
             res.status(200).json('<h3>Thank you for decline</h3>');
         }
@@ -102,19 +96,16 @@ addUserController.emailResponse = function(req, res) {
 
 //Get Account Detail
 addUserController.getAccountDetail = function(req, res) {
-    console.log("get addUser controller: ", req.body.userId);
     addUserController.getAccountlist(req.body.userId, res);
 };
 
 //Get Account List
 addUserController.getAccountlist= function(id, res ) {
-    console.log("get addUser controller: ",id);
     User.findOne({
         userid: id
     })
     .exec()
     .then(function (user) { 
-        console.log('user data: ', user)
         AccountAccessList.findOne({ 
             userObjectId: user._id 
         }).exec(function(err, accounts) {
@@ -122,7 +113,6 @@ addUserController.getAccountlist= function(id, res ) {
                 console.log('Error getting list of Chat', err);
                 res.status(401).json({error: 'Account Not found'})
             } else {
-                console.log("account list : ", accounts)
                 if(accounts){
                 res.status(200).json({accountList: accounts.accountAccessList}) 
                 } else {
@@ -133,11 +123,12 @@ addUserController.getAccountlist= function(id, res ) {
     });
 };
 
+//This method send email to user
 addUserController.emailSender= function(req, res, userInfo) {
-    console.log('user data and info: ', userInfo)
     let emailToken = jwt.sign(
         {
-            user: userInfo.userId
+            userId: userInfo.userId,
+            emailSend: userInfo.email
         },
         CONFIG.jwt_secret_key,
         {
@@ -168,68 +159,9 @@ addUserController.emailSender= function(req, res, userInfo) {
         res.status(200).json({successMessageId: '123445' });
         }
     });
-
-    //console.log('email message: ', sgMail);
 }
 
-//Send Email
-// addUserController.sendEmail= function(req, res, userInfo) {
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//         host: CONFIG.email_host,
-//         port: CONFIG.email_port,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: CONFIG.email_username, // generated ethereal user
-//             pass: CONFIG.email_password // generated ethereal password
-//         }
-//     });
-
-//     let emailToken = jwt.sign(
-//         {
-//             user: userInfo.name
-//         },
-//         CONFIG.jwt_secret_key,
-//         {
-//             expiresIn: '1d'
-//         }
-//     );
-//     console.log('token', 'http://localhost:3000/api/conformation/'+emailToken);
-//     // setup email data with unicode symbols
-//     let mailOptions = {
-//         from: CONFIG.email_username, // sender address
-//         to: userInfo.email, // list of receivers
-//         subject: 'Confirmation', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: `<b>${userInfo.name} want add you to his/her account.</b>
-//             </br> 
-//             <p>To give access please click to this link: </br>
-//             http://localhost:3000/api/conformation/${emailToken}
-//             </p>
-//             ` // html body
-//     };
-
-//     // send mail with defined transport object
-//     transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             console.log("Error while sending email:", error);
-//             //return addUserController.getAccountDetail(req,res);
-//             //  res.status(401).json({
-//             //     userHasAuthenticated:false,
-//             //     message:'Email send Failed!'
-//             // })
-            
-//         } else {
-//         res.status(200).json({successMessageId: info.messageId });
-//         console.log('Message sent: %s', info.messageId);
-//         // Preview only available when sending through an Ethereal account
-//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//         }
-
-//     });
-// };
-
-
+//Get list of approved account
 addUserController.getListOfAccount= function(req, res) {
     console.log("User Email : ",req.query.userId);
     User.findOne({
@@ -238,7 +170,7 @@ addUserController.getListOfAccount= function(req, res) {
     .exec()
     .then(function (user) { 
         console.log("User", user)
-        AccountAccessList.findOne({ 
+        AccountList.findOne({ 
             userObjectId:user._id  
         }).exec(function(err, accounts) { 
             if(err) {
@@ -246,7 +178,7 @@ addUserController.getListOfAccount= function(req, res) {
             } else {
                 if(accounts) {
                     console.log('User List', accounts)
-                    res.status(200).json({accountList: accounts.accountAccessList}) 
+                    res.status(200).json({accountList: accounts.accountList}) 
                 } else {
                     res.status(204).json({accountList:[]});
                 }
@@ -254,5 +186,58 @@ addUserController.getListOfAccount= function(req, res) {
         });
     });
 }
+
+//Update the AccountList json file with new approved account
+addUserController.updateAccount = function(userDetail, res) {
+    var note_dateUTC = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+
+    User.findOne({
+        userid: userDetail.userId
+    })
+    .exec()
+    .then(function (user) { 
+        AccountAccessList.findOne({
+            userObjectId: user._id 
+        })
+        .exec()
+        .then(function (accountDetail) {
+            var account = accountDetail.accountAccessList.find(function(item){
+                if(item.email === userDetail.emailSend) {
+                    return item;
+                }
+            });
+
+            if(account) {
+                let accountInfo = {
+                    accountList: {
+                        dateTime: {
+                            date:note_dateUTC,
+                            hour:moment.utc(note_dateUTC,'HH'),
+                            min:moment.utc(note_dateUTC,'mm')
+                        },
+                        email: account.email,
+                        userId : account.userId,
+                        fullName: account.fullName,
+                        authorizedLevel: account.authorizedLevel,
+                        relationShip: account.relationShip  
+                    }     
+                };
+
+                AccountList.findOneAndUpdate(            
+                    {userObjectId: user._id},
+                    {$push: accountInfo},
+                    {safe:true,upsert:true}
+                )
+                .exec()
+                .then(function (account) {
+                    console.log('account', account);
+                    res.status(200).json('<h3>Thank you for authorization</h3>');
+                });
+            }
+        });
+
+    });
+}
+
 
 module.exports = addUserController;
